71e7b6f2b430f07525cb68c5a2f956bc5fc2d8b9
Edidiong Ukem <edidionge.ukem@gmail.com>
2022-09-08 20:29:10 -0700

    task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- *   
+ * Description: to standard error  
  * Return: Always 1 (Error)
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
