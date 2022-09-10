3db1ea5f7ee22577e276579983ff3a4c0595a507
Edidiong Ukem <edidionge.ukem@gmail.com>
2022-09-09 18:18:31 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase and then in uppercase, followed by a new line
+ * main - prints the alphabet in lowercase and then in uppercase,
+ * followed by a new line
  * Return: 0 (success)
  */
 int main(void)
