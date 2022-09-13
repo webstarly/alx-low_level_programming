1d7eaf9ffd31f3d79395669598f344b16efb4a3d
Edidiong Ukem <edidionge.ukem@gmail.com>
2022-09-13 15:59:12 -0700

    prints alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * print_alphabet -  prints the alphabet, in lowercase, followed by a new line
+ * 
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
