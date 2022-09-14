dad590b716e95604495bc8065bb25cf3b439ba1a
Edidiong Ukem <edidionge.ukem@gmail.com>
2022-09-13 19:39:49 -0700

    prints 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- *
- * print_alphabet_x10 -> prints the lowercase alphabets
- *
+ * print_alphabet_x10 - prints the lowercase alphabets
  */
 void print_alphabet_x10(void)
 {
