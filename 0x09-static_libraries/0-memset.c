185fa9d1503ab387269b68ebfdd5ee1098e8b5cd
Edidiong Ukem <edidionge.ukem@gmail.com>
2022-09-30 18:34:32 -0700

    Create the static library libmy.a

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * * _memset - memory set function
+ *
+ * * @s: string
+ *
+ * * @b: character
+ *
+ * * @n: integer
+ *
+ * * Return: string
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[a] = b;
+	return (s);
+}
