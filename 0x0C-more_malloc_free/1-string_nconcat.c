454e27e70694c5f987edc4313b32bec2af7ff0b8
Edidiong Ukem <edidionge.ukem@gmail.com>
2022-10-06 15:58:30 -0700

    concatenants two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
  *
  * * Return: pointer to the allocated memory.
  *
- * *if malloc fails, status value is equal to 98.
+ * * if malloc fails, status value is equal to 98.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
