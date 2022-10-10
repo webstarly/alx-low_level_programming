34922f7f5905d021fdd368367fed02bdc566013a
Edidiong Ukem <edidionge.ukem@gmail.com>
2022-10-10 16:56:21 -0700

    initialize a variable of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,11 @@ typedef struct dog dog_t;
  *
  * @owner: owner of the dog
  *
- * Description: struct called "dog" that stores its name, its age
+ * struct called "dog" that stores its name, its age
  *
  * and the name of its owner.
  */
+
 struct dog
 {
 	char *name;
